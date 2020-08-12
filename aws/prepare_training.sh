4e28a769d8a2a7ddfd94edb57061bfb55341dde9
naomori <naoki.morita@gmail.com>
2020-08-13 07:55:22 +0900

    update

--- aws/prepare_training.sh
+++ aws/prepare_training.sh
@@ -12,6 +12,8 @@ chmod 755 /content/yolov5/scripts/*.sh
 
 rsync -ahv --progress /s3/runs-2020-08-05-epoch300/runs/exp0/weights/last.pt            /content/yolov5/weights/yolov5s-last-300.pt
 rsync -ahv --progress /s3/runs-2020-08-10-yolov5m-epoch100/runs/exp0/weights/last.pt    /content/yolov5/weights/yolov5m-last-100.pt
+rsync -ahv --progress /s3/runs-2020-08-11-yolov5m-epoch100+100/exp0/weights/last.pt     /content/yolov5/weights/yolov5m-last-200.pt
+rsync -ahv --progress /s3/runs-2020-08-12-yolov5m-epoch100+100+100/exp0/weights/last.pt /content/yolov5/weights/yolov5m-last-300.pt
 rsync -ahv --progress /s3/runs-yolov5x-2020-08-09-epoch50/runs/exp0/weights/last.pt     /content/yolov5/weights/yolov5x-last-50.pt
 chmod 664 /content/yolov5/weights/*.pt
 
--- aws/yolov5x_train.sh
+++ aws/yolov5x_train.sh
@@ -1,16 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
 img=640
 batch=16
 epoch_num=50
 model=yolov5x
-weight=yolov5x-last-50.pt
+resume=true
+weights=./weights/yolov5x-last-50.pt
 
-python3 train.py --img ${img} --batch ${batch} --epochs ${epoch_num} \
-    --data ./config/fdlpd_colab.yaml \
-    --cfg ./models/${model}.yaml \
-    --weights ./weights/${weight} \
-    --cache-images
 
-date=`date -I`
-rsync -ahv --progress ./runs /s3/runs-${date}-${model}-epoch100
+function get_train_cmd() {
+    cmd="python3 train.py "
+    cmd+=" --img ${img} --batch ${batch} --epochs ${epoch_num}"
+    cmd+=" --data ./config/fdlpd_colab.yaml"
+    cmd+=" --cfg ./models/${model}.yaml"
+    cmd+=" --cache-images"
+    if [ ${resume} ]; then
+        cmd+=" --resume ${weights} --weights ${weights}"
+    else
+        cmd+=" --weights ${weights}"
+    fi
+    echo "${cmd}"
+    return
+}
+
+function get_latest_exp_dir() {
+    find_dir=$1
+    latest_file=$(find "${find_dir}" -type f -name "last*.pt" -print0 | xargs -0 ls -t | head -n 1)
+    latest_dir=${latest_file%/*}
+    parent_dir=${latest_dir%/*}
+    echo "${parent_dir}"
+    return
+}
+
+cmd=$(get_train_cmd)
+${cmd}
+
+exp_dir=$(get_latest_exp_dir "./runs")
+date=$(date -I)
+
+rsync -ahv --progress "${exp_dir}" "/s3/runs-${date}-${model}-epoch${epoch_num}+50"
